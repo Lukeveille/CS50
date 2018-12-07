efd33ac6b2b3ffdc1e102508facad42afef16ccc
Lukeveille <luke@lukeleveille.com>
2018-12-07 00:12:18 -0500

    typo

--- sorting.rb
+++ sorting.rb
@@ -92,3 +92,26 @@ loop do
 end
 
 puts "cocktail sort done in #{it} steps"
+
+# insertion sort
+
+# merge sort
+numbers = initials.dup
+
+mergersort(numbers)
+
+def mergesort(array)
+  return array if array.length == 1
+  middle = array.length / 2
+  merge mergesort(array[0...middle]), mergesort(array[middle..-1])
+end
+
+def merge(left, right)
+  result = []
+  until left.length == 0 || right.length == 0 do
+    result << (left.first <= right.first ? left.shift : right.shift)
+  end
+  result + left + right
+end
+
+puts "cocktail sort done in #{it} steps"
